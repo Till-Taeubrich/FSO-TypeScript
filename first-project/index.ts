1e3a979c1c5d46081c6b0953eaae75992aba054a
Till Taeubrich <till.taeubrich@gmail.com>
2023-09-09 20:54:27 +0200

    finish exercise 9.4

---
+++ first-project/index.ts
@@ -0,0 +1,12 @@
+import express = require('express')
+const app = express()
+
+app.get('/', (_request, response) => {
+  response.send('<h1>Hello Full Stack!</h1>')
+})
+
+const PORT = 3001
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
