a01a482bb11fb2f1d52f3f3fefd0bf2c002c64ee
Till Taeubrich <till.taeubrich@gmail.com>
2023-09-27 20:28:01 +0300

    finish until next exercises

--- patientor/patientor-backend/src/types.ts
+++ patientor/patientor-backend/src/types.ts
@@ -39,3 +39,28 @@ export const isGender = (param: string): param is Gender => {
 export type patientsEntryNoSsn = Omit<patientsEntry, 'ssn'>
 
 export type NonSensitivePatient = Omit<patientsEntry, 'ssn' | 'entries'>;
+
+interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<diagnoseEntry['code']>;
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+export type Entry =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
